baf7677c119de15b5b789fa3f3bc8782c37d3906
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-10-31 19:21:42 +0100

    import ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnnouncementComponent } from './announcement/announcement.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,12 +15,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
 	declarations: [AppComponent, AnnouncementComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
+		HttpClientModule,
+		ReactiveFormsModule,
 		BrowserAnimationsModule,
 		MatToolbarModule,
 		MatIconModule,
@@ -27,6 +32,7 @@ import { MatTableModule } from '@angular/material/table';
 		MatInputModule,
 		MatButtonModule,
 		MatTableModule,
+		MatSnackBarModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
